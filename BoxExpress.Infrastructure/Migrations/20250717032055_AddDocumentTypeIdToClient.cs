81d262857cf401c09778b109d70a77b900045d81
jsalinasninja <jsalinas@ninjaexcel.com>
2025-07-17 18:53:51 -0500

    progress on create

---
+++ BoxExpress.Infrastructure/Migrations/20250717032055_AddDocumentTypeIdToClient.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BoxExpress.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDocumentTypeIdToClient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DocumentTypeId",
+                table: "Client",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Client_DocumentTypeId",
+                table: "Client",
+                column: "DocumentTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Client_DocumentTypes_DocumentTypeId",
+                table: "Client",
+                column: "DocumentTypeId",
+                principalTable: "DocumentTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Client_DocumentTypes_DocumentTypeId",
+                table: "Client");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Client_DocumentTypeId",
+                table: "Client");
+
+            migrationBuilder.DropColumn(
+                name: "DocumentTypeId",
+                table: "Client");
+        }
+    }
+}
