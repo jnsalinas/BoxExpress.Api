7b7f08c0f085e9bf545d3bc6cdadf40e6cb61a28
jsalinasninja <jsalinas@ninjaexcel.com>
2025-08-22 17:32:57 -0500

    migrations

--- BoxExpress.Infrastructure/Migrations/20250822222940_ShopifyData.Designer.cs
+++ BoxExpress.Infrastructure/Migrations/20250822222940_ShopifyData.Designer.cs
@@ -4,6 +4,7 @@ using BoxExpress.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BoxExpress.Infrastructure.Migrations
 {
     [DbContext(typeof(BoxExpressDbContext))]
-    partial class BoxExpressDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250822222940_ShopifyData")]
+    partial class ShopifyData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BoxExpress.Infrastructure/Migrations/20250822222940_ShopifyData.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BoxExpress.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ShopifyData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ShopifyAccessToken",
+                table: "Stores",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShopifyShopDomain",
+                table: "Stores",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShopifyAccessToken",
+                table: "Stores");
+
+            migrationBuilder.DropColumn(
+                name: "ShopifyShopDomain",
+                table: "Stores");
+        }
+    }
+}
